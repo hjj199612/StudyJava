03678d13517f1a9691d9380e3f6599e5c66d6795
hjj199612 <hjj199612@qq.com>
2021-07-26 21:41:17 +0800

    每日一题

--- StudyJava/src/leetCode/Solution_1713.java
+++ StudyJava/src/leetCode/Solution_1713.java
@@ -1,9 +1,13 @@
 package leetCode;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author huangjj24
  * @date 2021-7-26 9:10
- *
  **/
 public class Solution_1713 {
 
@@ -12,6 +16,40 @@ public class Solution_1713 {
     }
 
     public int minOperations(int[] target, int[] arr) {
-        return 0;
+        int n = target.length;
+        Map<Integer, Integer> pos = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            pos.put(target[i], i);
+        }
+        List<Integer> d = new ArrayList<Integer>();
+        for (int val : arr) {
+            if (pos.containsKey(val)) {
+                int idx = pos.get(val);
+                int it = binarySearch(d, idx);
+                if (it != d.size()) {
+                    d.set(it, idx);
+                } else {
+                    d.add(idx);
+                }
+            }
+        }
+        return n - d.size();
+    }
+
+    public int binarySearch(List<Integer> d, int target) {
+        int size = d.size();
+        if (size == 0 || d.get(size - 1) < target) {
+            return size;
+        }
+        int low = 0, high = size - 1;
+        while (low < high) {
+            int mid = (high - low) / 2 + low;
+            if (d.get(mid) < target) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
     }
 }
